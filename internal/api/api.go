72ee2d756bd14abb39f31f1de1d5e4e387721580
Matt Massoodi <m.massoodi@gmail.com>
2026-06-08 09:13:17 -0700

    clean and add db support to api

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -8,10 +9,28 @@ import (
 	"time"
 
 	"github.com/lutia-io/huma/pkg/middleware"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo/readpref"
 )
 
 func New() {
 	logger := slog.Default()
+
+	uri := os.Getenv("API_SERVICE_MONGO_URI")
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		logger.Error("failed to connect to mongo", "error", err)
+	}
+	defer func() {
+		if err := client.Disconnect(context.Background()); err != nil {
+			logger.Error("failed to disconnect from mongo", "error", err)
+		}
+	}()
+
+	if err := client.Ping(context.Background(), readpref.Primary()); err != nil {
+		logger.Error("failed to ping mongo", "error", err)
+	}
 
 	mux := http.NewServeMux()
 	handler := http.Handler(mux)
