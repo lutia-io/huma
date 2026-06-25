f7c45b80d563ef668b2f4a45c3b6a0c8da285d8e
Matt Massoodi <m.massoodi@gmail.com>
2026-06-24 20:48:43 -0700

    atlas schema migrations clean up

--- helm/migrate/sql/0000002_networks.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.networks (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name TEXT NOT NULL,
-    slug TEXT UNIQUE NOT NULL,
-    user_id UUID NOT NULL REFERENCES public.users(id),
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL,
-    deleted_at TIMESTAMPTZ
-);
