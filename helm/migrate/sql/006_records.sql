27621db903d079e22c5f16648374dbc040544d1e
Matt Massoodi <m.massoodi@gmail.com>
2026-08-09 23:35:08 -0700

    oops

--- helm/migrate/sql/006_records.sql
+++ helm/migrate/sql/006_records.sql
@@ -4,6 +4,7 @@ CREATE TABLE public.records (
     schema_id UUID NOT NULL REFERENCES public.schemas(id),
     organization_id UUID NOT NULL REFERENCES public.organizations(id),
     organization_user_id UUID NOT NULL REFERENCES public.organization_users(id),
+    network_id UUID NOT NULL REFERENCES public.networks(id),
     idempotency_key TEXT,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL,
